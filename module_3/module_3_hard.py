c251c841335d048c048865c3f9d10c2808359e08
Alex <zabotkinjr@gmail.com>
2024-08-23 22:34:35 +0300

    23.08

---
+++ module_3/module_3_hard.py
@@ -0,0 +1,44 @@
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+data = data_structure
+list_ = []
+
+def choice():
+    for i in data_structure:
+        if isinstance(i, list):
+            list_append()
+            continue
+        elif isinstance(i, dict):
+            list_append()
+            continue
+        elif isinstance(i, set):
+            list_append()
+            continue
+        elif isinstance(i, tuple):
+            list_append()
+            continue
+        elif isinstance(i, set):
+            list_append()
+            continue
+        else:
+            continue
+
+def list_append():
+        if isinstance(j, int):
+            list_.append(j)
+        if isinstance(j, str):
+            list_.append(j)
+
+
+def count(*args):
+
+
+
+def calculate_structure_sum(data_structure):
+
