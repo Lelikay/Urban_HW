6138376f707d9e55848045de205ec75af6c011bb
Alex <zabotkinjr@gmail.com>
2024-08-25 17:01:35 +0300

    25.08

--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -1,44 +1,33 @@
+def calculate_structure_sum(args):
+    total = 0
+
+    def recursive_sum(args):
+        nonlocal total
+
+        for item in args:
+            if isinstance(item, int):
+                total += item
+            elif isinstance(item, str):
+                total += len(item)
+            elif isinstance(item, list):
+                recursive_sum(item)
+            elif isinstance(item, tuple):
+                recursive_sum(item)
+            elif isinstance(item, dict):
+                for key, value in item.items():
+                    recursive_sum([key, value])
+
+    recursive_sum(args)
+
+    return total
+
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-data = data_structure
-list_ = []
-
-def choice():
-    for i in data_structure:
-        if isinstance(i, list):
-            list_append()
-            continue
-        elif isinstance(i, dict):
-            list_append()
-            continue
-        elif isinstance(i, set):
-            list_append()
-            continue
-        elif isinstance(i, tuple):
-            list_append()
-            continue
-        elif isinstance(i, set):
-            list_append()
-            continue
-        else:
-            continue
-
-def list_append():
-        if isinstance(j, int):
-            list_.append(j)
-        if isinstance(j, str):
-            list_.append(j)
-
-
-def count(*args):
-
-
-
-def calculate_structure_sum(data_structure):
-
+result = calculate_structure_sum(data_structure)
+print(result)
