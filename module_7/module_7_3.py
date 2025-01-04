e5479b4a6d883cf8578e6ac046c9689dee64dffc
Alex <zabotkinjr@gmail.com>
2025-01-04 11:27:12 +0300

    04.01

--- module_7/module_7_3.py
+++ module_7/module_7_3.py
@@ -1,6 +1,9 @@
 class WordsFinder:
     file_names = []
 
-    def __init__(self, name):
-        self.name = name
-        file_names
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def get_all_words (self, all_words):
+        all_words = {}
+        with open(file_names) as file
